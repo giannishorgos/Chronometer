34449c282c6fdb07b80e3c210a486fdc98e16074
giannishorgos <giannis.21.osfp@gmail.com>
2020-06-18 15:58:29 +0300

    Added stop button

--- main.js
+++ main.js
@@ -1,39 +1,52 @@
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-  }
+}
 
-async function clock()
-{   
-    let hours = 0;
-    let minutes = 0;
-    let seconds = 0;
-    let str = "";
-    let tag;
-    let start = document.getElementById("start");
-    start.classList.add("hide");
-    while(true)
-    {
+  async function clock()
+  {   
+      let hours = 0;
+      let minutes = 0;
+      let seconds = 0;
+      let str = "00:00:00";
+      let tag;
+      let start = document.getElementById("start");
+      start.classList.add("hide");
+      let stop = document.getElementById("stop");
+      stop.classList.remove("hide");
+      while(true)
+      {
         tag = document.getElementById("re");
         tag.innerHTML = str;
-       seconds++;
-       if(seconds === 60) {
+        seconds++;
+        if(seconds === 60) {
             seconds = 0;
             minutes++;
-       }
-       if(minutes === 60) {
-           minutes = 0;
-           hours++;
-       }
-       if(hours === 24)
-            hours = 0;
+        }
+        if(minutes === 60) {
+            minutes = 0;
+            hours++;
+        }
+        if(hours === 24)
+        hours = 0;
         
         str = ((hours < 10) ? "0" + hours : hours) + ":"
         + ((minutes < 10) ? "0" + minutes : minutes) + ":"
         + ((seconds < 10) ? "0" + seconds : seconds);
         await sleep(1000);
         
+        stop.addEventListener("click", return);
     }
 }
 
-const start = document.getElementById("start");
+function reset()
+{
+    let str = "00:00:00";
+    let tag = document.getElementById("re");
+    tag.innerText = str;
+}
+
+
+let start = document.getElementById("start")
 start.addEventListener("click", clock);
+
